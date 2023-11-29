d7ad57dbffc9eaa39a9da6378764bd6ee8110e77
moonbeom0115 <moona0115@naver.com>
2023-11-29 14:05:38 +0000

    frontend initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Router from 'svelte-spa-router'
+	import { location } from 'svelte-spa-router'
+	import Home from "./routes/Home.svelte"
+	import Detail from "./routes/Detail.svelte"
+    import QuestionCreate from "./routes/QuestionCreate.svelte"
+    import Navigation from "./components/Navigation.svelte"
+    import UserCreate from "./routes/UserCreate.svelte"
+    import UserLogin from "./routes/UserLogin.svelte"
+    
+    import { is_login } from "./lib/store"
+	
+	const routes = {
+		'/': Home,
+		'/detail/:question_id' : Detail,
+        '/question-create': QuestionCreate,
+        '/user-create': UserCreate,
+        '/user-login': UserLogin,
+	}
+
+</script>
+
+<Navigation />     
+<Router {routes}/>
